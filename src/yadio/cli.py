95e58285bfad842a1215899aa4084c88336ed1ae
as <cv0028101@gamil.com>
2025-09-29 18:27:29 -0400

    Primer commit

--- src/yadio/cli.py
+++ src/yadio/cli.py
@@ -2,6 +2,7 @@ import typer
 
 app = typer.Typer(help="Mi app de ejemplo con Typer")
 
+
 @app.command()
 def saludar(nombre: str):
     """
@@ -9,13 +10,15 @@ def saludar(nombre: str):
     """
     typer.echo(f"¡Hola, {nombre}!")
 
+
 @app.command()
 def saludar_rep(nombre: str, repetir: int = 1):
     """
     Saluda varias veces a alguien.
     """
     for i in range(repetir):
-        typer.echo(f"¡Hola ({i+1}) {nombre}!")
+        typer.echo(f"¡Hola ({i + 1}) {nombre}!")
+
 
 if __name__ == "__main__":
     app()
--- src/yadio/yadio.py
+++ src/yadio/yadio.py
@@ -1,24 +1,44 @@
 import httpx
-from typing import Dict
+from typing import Dict, Any
 
-class YadioApi():
+class YadioApi:
+    """SDK simple para la API de Yadio"""
 
-    def __init__(self):
-        self.base_url= "https://api.yadio.io/"
-    def _request(self, enpoint : str) -> Dict:
-        url=f"{self.base_url}{enpoint}"
-        requests= httpx.get(url)
-        requests.raise_for_status()
-        
-        return requests.json()
+    BASE_URL = "https://api.yadio.io/"
+
+    @classmethod
+    def _request(cls, endpoint: str) -> Dict[str, Any]:
+        url = f"{cls.BASE_URL}{endpoint}"
+        response = httpx.get(url, timeout=5)
+        response.raise_for_status()
+        return response.json()
+
+    @classmethod
+    def ping_server(cls) -> Dict[str, Any]:
+        """
+Checks the server status.
+       
+        """
+        return cls._request("ping")
+
+    @classmethod
+    def get_rate(cls, quote: str, base: str) -> Dict[str, Any]:
+        """Returns the current exchange rate for a specific currency pair."""
+        return cls._request(f"rate/{quote}/{base}")
+
+    @classmethod
+    def get_exrates(cls, currency: str = "USD") -> Dict[str, Any]:
+        """Retrieves the current foreign exchange rates, using USD as the default base currency."""
+        return cls._request(f"exrates/{currency}")
+
+    @classmethod
+    def get_currencies(cls) -> Dict[str, Any]:
+        """Returns a list of all available currencies."""
+        return cls._request("currencies")
+
+    @classmethod
+    def get_convert(cls, amount: float, currency_from: str, currency_to: str) -> Dict[str, Any]:
+        "Converts a specified amount from a source currency to a target currency."
+        return cls._request(f"convert/{amount}/{currency_from}/{currency_to}")
 
 
-    def ping_server(self):
-        return self._request("ping")
-    def get_convert(self, amount: float , currency_from : str , currency_to: str):
-        return self._request(f"convert/{amount}/{currency_from}/{currency_to}")
-
-
-cliente= YadioApi()
-
-print(cliente.get_convert(1,"USD", "CUP"))
